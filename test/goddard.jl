7efd24256337c8bbbed2a5da2969292b75881540
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-08-28 00:17:40 +0200

    linear solver = mumps for CPU

--- test/goddard.jl
+++ test/goddard.jl
@@ -43,13 +43,50 @@ o = @def begin
 
 end
 
+# Quadrotor
+
+const T = 60
+const g = 9.8
+const r = 0.1
+
+o2 = @def begin 
+
+    t ∈ [0, T], time
+    x ∈ R^10, state
+    u ∈ R⁴, control
+
+    x(0) == zeros(10)
+
+    ∂(x₁)(t) == x₂(t)
+    ∂(x₂)(t) == u₁(t) * cos(x₇(t)) * sin(x₈(t)) * cos(x₉(t)) + u₁(t) * sin(x₇(t)) * sin(x₉(t))
+    ∂(x₃)(t) == x₄(t)
+    ∂(x₄)(t) == u₁(t) * cos(x₇(t)) * sin(x₈(t)) * sin(x₉(t)) - u₁(t) * sin(x₇(t)) * cos(x₉(t))
+    ∂(x₅)(t) == x₆(t)
+    ∂(x₆)(t) == u₁(t) * cos(x₇(t)) * cos(x₈(t)) - g 
+    ∂(x₇)(t) == u₂(t) * cos(x₇(t)) / cos(x₈(t)) + u₃(t) * sin(x₇(t)) / cos(x₈(t))
+    ∂(x₈)(t) ==-u₂(t) * sin(x₇(t)) + u₃(t) * cos(x₇(t))
+    ∂(x₉)(t) == u₂(t) * cos(x₇(t)) * tan(x₈(t)) + u₃(t) * sin(x₇(t)) * tan(x₈(t)) + u₄(t)
+
+    dt1 = sin(2π * t / T)
+    df1 = 0 
+    dt3 = 2sin(4π * t / T)
+    df3 = 0 
+    dt5 = 2t / T
+    df5 = 2
+
+    ∂(x[10])(t) == (x₁(t) - dt1)^2 + (x₃(t) - dt3)^2 + (x₅(t) - dt5)^2 + x₇(t)^2 + x₈(t)^2 + x₉(t)^2 +
+                   r * ( u₁(t)^2 + u₂(t)^2 + u₃(t)^2 + u₄(t)^2 )
+    0.5( (x₁(T) - df1)^2 + (x₃(T) - df3)^2 + (x₅(T) - df5)^2 + x₇(T)^2 + x₈(T)^2 + x₉(T)^2 )+ 0.5x[10](T) → min
+
+end
+
 # Solving
 
 tol = 1e-7
-#print_level = MadNLP.WARN
-print_level = MadNLP.INFO
+print_level = MadNLP.WARN
+#print_level = MadNLP.INFO
 
-for N ∈ (100,) # 500, 1000, 2000, 5000, 7500, 10000, 20000, 50000)
+for N ∈ (100, 200, 500, 750, 1000, 2000, 5000, 7500, 10000, 20000, 50000, 75000, 100000)
 
     m_cpu = model(direct_transcription(o, :exa; grid_size=N))
     m_gpu = model(direct_transcription(o, :exa; grid_size=N, exa_backend=CUDABackend()))
